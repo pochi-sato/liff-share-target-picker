efbc099e19fa9234d52ef9473a27127702744c5c
Takuto Sato <foreignduck.domesticduck@gmail.com>
2020-05-05 11:27:41 +0900

    .

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// declare module '*.png';
+// declare module '*.jpg';
+// declare module '*.jpeg';
+// declare module '*.gif';
+// declare module 'windwo';
+
+declare const window: Window | any | undefined;
